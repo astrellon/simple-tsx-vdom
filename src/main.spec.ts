1b942ce5a1fb60cc19f92e4542c5fdb46c532e13
astrellon <sovereign250@gmail.com>
2021-02-13 23:19:18 +1100

    Added tests for class components.

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -4,7 +4,6 @@ test('basic test', () =>
 {
     document.body.innerHTML = '<main id="root"></main>';
     const rootEl = document.getElementById('root');
-    console.log('Root El', rootEl);
     const vdom1 = vdom('main', {id: 'mainId'},
         vdom('h1', {id: 'headerId'}, 'Hello'),
         vdom('p', {id: 'paragraphId'}, 'Whats up?')
@@ -40,7 +39,7 @@ test('basic test', () =>
     expect(paragraphEl.innerHTML).toBe('Whats is up?');
 });
 
-test('test class component', () =>
+test('test class component with key', () =>
 {
     interface Props
     {
@@ -70,7 +69,6 @@ test('test class component', () =>
 
     document.body.innerHTML = '<main id="root"></main>';
     const rootEl = document.getElementById('root');
-    console.log('Root El', rootEl);
     const vdom1 = vdom('main', {id: 'mainId'},
         vdom(TestNode, {key: 'node1', name: 'Foo'}),
         vdom(TestNode, {key: 'node2', name: 'Bar'}),
@@ -156,4 +154,182 @@ test('test class component', () =>
     expect(getChild(2).innerHTML).toBe('TestNode: Foo');
     expect(getChild(3).nodeName).toBe('STRONG');
     expect(getChild(3).innerHTML).toBe('TestNode: Boop');
-})
+});
+
+test('test class component without key', () =>
+{
+    interface Props
+    {
+        readonly name: string;
+    }
+
+    const createdNodes: TestNode[] = [];
+
+    class TestNode extends VDomComponent<Props>
+    {
+        public renderCount = 0;
+        constructor()
+        {
+            super();
+            createdNodes.push(this);
+        }
+
+        public render(): VirtualElement
+        {
+            this.renderCount++;
+
+            const { name } = this.props;
+
+            return vdom(name[0] === 'B' ? 'strong' : 'span', {}, 'TestNode: ', name);
+        }
+    }
+
+    document.body.innerHTML = '<main id="root"></main>';
+    const rootEl = document.getElementById('root');
+    const vdom1 = vdom('main', {id: 'mainId'},
+        vdom(TestNode, { name: 'Foo' }),
+        vdom(TestNode, { name: 'Bar' }),
+        vdom(TestNode, { name: 'Baz' })
+    );
+
+    if (rootEl == null) { fail('Root element not created!'); }
+
+    render(vdom1, rootEl);
+
+    const mainEl = document.getElementById('mainId');
+    if (mainEl == null) { fail('Main element not created!'); }
+
+    const getChild = (index: number) =>
+    {
+        return mainEl.children.item(index) as Element;
+    }
+
+    expect(createdNodes.length).toBe(3);
+
+    expect(createdNodes[0].renderCount).toBe(1);
+    expect(createdNodes[0].props.name).toBe('Foo');
+    expect(createdNodes[1].renderCount).toBe(1);
+    expect(createdNodes[1].props.name).toBe('Bar');
+    expect(createdNodes[2].renderCount).toBe(1);
+    expect(createdNodes[2].props.name).toBe('Baz');
+
+    expect(mainEl.children.length).toBe(3);
+    expect(getChild(0).nodeName).toBe('SPAN');
+    expect(getChild(0).innerHTML).toBe('TestNode: Foo');
+    expect(getChild(1).nodeName).toBe('STRONG');
+    expect(getChild(1).innerHTML).toBe('TestNode: Bar');
+    expect(getChild(2).nodeName).toBe('STRONG');
+    expect(getChild(2).innerHTML).toBe('TestNode: Baz');
+
+    const vdom2 = vdom('main', {id: 'mainId'},
+        vdom(TestNode, { name: 'Baz' }),
+        vdom(TestNode, { name: 'Bar' }),
+        vdom(TestNode, { name: 'Foo' })
+    );
+    render(vdom2, rootEl);
+
+    expect(createdNodes.length).toBe(3);
+    expect(createdNodes[0].renderCount).toBe(2);
+    expect(createdNodes[0].props.name).toBe('Baz');
+    expect(createdNodes[1].renderCount).toBe(1);
+    expect(createdNodes[1].props.name).toBe('Bar');
+    expect(createdNodes[2].renderCount).toBe(2);
+    expect(createdNodes[2].props.name).toBe('Foo');
+
+    expect(mainEl.children.length).toBe(3);
+    expect(getChild(0).nodeName).toBe('STRONG');
+    expect(getChild(0).innerHTML).toBe('TestNode: Baz');
+    expect(getChild(1).nodeName).toBe('STRONG');
+    expect(getChild(1).innerHTML).toBe('TestNode: Bar');
+    expect(getChild(2).nodeName).toBe('SPAN');
+    expect(getChild(2).innerHTML).toBe('TestNode: Foo');
+
+    const vdom3 = vdom('main', {id: 'mainId'},
+        vdom(TestNode, { name: 'Baz' }),
+        vdom(TestNode, { name: 'A new name' }),
+        vdom(TestNode, { name: 'Foo' }),
+        vdom(TestNode, { name: 'Boop' }),
+    );
+    render(vdom3, rootEl);
+
+    expect(createdNodes.length).toBe(4);
+    expect(createdNodes[0].renderCount).toBe(2);
+    expect(createdNodes[0].props.name).toBe('Baz');
+    expect(createdNodes[1].renderCount).toBe(2);
+    expect(createdNodes[1].props.name).toBe('A new name');
+    expect(createdNodes[2].renderCount).toBe(2);
+    expect(createdNodes[2].props.name).toBe('Foo');
+    expect(createdNodes[3].renderCount).toBe(1);
+    expect(createdNodes[3].props.name).toBe('Boop');
+
+    expect(mainEl.children.length).toBe(4);
+    expect(getChild(0).nodeName).toBe('STRONG');
+    expect(getChild(0).innerHTML).toBe('TestNode: Baz');
+    expect(getChild(1).nodeName).toBe('SPAN');
+    expect(getChild(1).innerHTML).toBe('TestNode: A new name');
+    expect(getChild(2).nodeName).toBe('SPAN');
+    expect(getChild(2).innerHTML).toBe('TestNode: Foo');
+    expect(getChild(3).nodeName).toBe('STRONG');
+    expect(getChild(3).innerHTML).toBe('TestNode: Boop');
+});
+
+test('event handling', () =>
+{
+    interface Props
+    {
+        readonly name: string;
+    }
+
+    class TestNode extends VDomComponent<Props>
+    {
+        public render(): VirtualElement
+        {
+            const { name } = this.props;
+
+            return vdom(name[0] === 'B' ? 'strong' : 'span', {onclick: () => clicked2++ }, 'TestNode: ', name);
+        }
+    }
+
+    document.body.innerHTML = '<main id="root"></main>';
+    const rootEl = document.getElementById('root');
+    let clicked1 = 0;
+    let clicked2 = 0;
+
+    const vdom1 = vdom('main', {id: 'mainId'},
+        vdom('div', {onclick: () => { clicked1++; }}),
+        vdom(TestNode, {name: 'Foo'})
+    );
+
+    if (rootEl == null) { fail('Root element not created!'); }
+
+    render(vdom1, rootEl);
+
+    const mainEl = document.getElementById('mainId');
+    if (mainEl == null) { fail('Main element not created!'); }
+
+    const getChild = (index: number) =>
+    {
+        return mainEl.children.item(index) as HTMLElement;
+    }
+
+    getChild(0).click();
+    getChild(0).click();
+    getChild(1).click();
+
+    expect(clicked1).toBe(2);
+    expect(clicked2).toBe(1);
+
+    const vdom2 = vdom('main', {id: 'mainId'},
+        vdom('div', {onclick: () => { clicked1++; }}),
+        vdom(TestNode, {name: 'Bar'})
+    );
+
+    render(vdom2, rootEl);
+
+    getChild(0).click();
+    getChild(0).click();
+    getChild(1).click();
+
+    expect(clicked1).toBe(4);
+    expect(clicked2).toBe(2);
+});
